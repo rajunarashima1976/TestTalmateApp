416c55a2d604cf7264673a16b49327bb5ed708b2
User6 <User6@CBAPOOL-0>
2020-06-22 13:53:47 +0000

    Added a new class TestCls and created properties

---
+++ CBA.Training.Talmate.Common/TestCls.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CBA.Training.Talmate.Common
+{
+    public class TestCls
+    {
+        public int empid1 { set; get; }
+        public string empname { set; get; }
+    }
+}
