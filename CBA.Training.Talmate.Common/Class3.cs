2908ea88ff35185bd610ce99360cc0a6fa7d5b36
User6 <User6@CBAPOOL-0>
2020-06-22 18:55:39 +0000

    Added a new class with empaddress property

---
+++ CBA.Training.Talmate.Common/Class3.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CBA.Training.Talmate.Common
+{
+    public class Class3
+    {
+        public string empaddress { set; get; }
+    }
+}
